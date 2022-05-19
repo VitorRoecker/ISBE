362a6c6cf0e8dbcc9781201cb24edc438b1a6d3d
Vitor Adriel <vitoradrielreocker@gmail.com>
2022-05-19 00:00:53 -0300

    Adicionado get, update e delete na controller

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -21,5 +21,49 @@ module.exports = {
         response.isSuccessful = affectRows > 0;
 
         return res.json(response);
+    },
+
+    async getPessoa(req, res) {
+        const response = { ...responseModel };
+
+        const { codigoPessoa } = req.body
+
+        const [, data] = await connection.query(`
+            SELECT * FROM Pessoa WHERE Pes_Codigo = ${codigoPessoa}
+        `);
+
+        response.isSuccessful = data != null
+
+        response.data = data;
+
+        return res.json(response);
+    },
+
+    async updatePessoa(req, res) {
+        const response = { ...responseModel };
+
+        const { pessoa } = req.body
+
+        const [, affectRows] = await connection.query(`
+            UPDATE Pessoa SET Pes_Nome = '${pessoa.nome}', Pes_Documento = '${pessoa.documento}', Pes_DataNascimento = '${pessoa.dataNascimento}', Pes_Telefone = '${pessoa.telefone}', Pes_TipoPessoa = '${pessoa.tipoPessoa}', Pes_DataInclusao = '${pessoa.dataInclusao}', 
+        `);
+
+        response.isSuccessful = affectRows > 0;
+
+        return res.json(response);
+    },
+
+    async deletePessoa(req, res){
+        const response = { ...responseModel };
+
+        const { codigoPessoa } = req.body;
+
+        const [, affectRows] = await connection.query(`
+            DELETE FROM Pessoa WHERE Pes_Codigo = ${codigoPessoa}
+        `);
+
+        resresponse.isSuccessful = affectRows > 0;
+
+        return res.json(response);
     }
 };
